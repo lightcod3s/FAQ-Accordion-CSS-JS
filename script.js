d9909e0ff91408e7376d242c6c70888706c8e467
Marvellous Bankole <152821703+lightcod3s@users.noreply.github.com>
2024-02-02 14:10:48 +0100

    added styling and interaction, still editing it.

---
+++ script.js
@@ -0,0 +1,45 @@
+// const items = document.querySelectorAll(".accordion button");
+
+// function toggleAccordion() {
+//   const itemToggle = this.getAttribute('aria-expanded');
+
+//   for (i = 0; i < items.length; i++) {
+//     items[i].setAttribute('aria--expanded', 'false');
+//   }
+
+//   if (itemToggle == 'false') {
+//     this.setAttribute('aria-expanded', 'true')
+//   }
+// }
+
+// items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+let count = 1;
+
+function toggleCount() {
+  count = (count === 1) ? -1 : 1;
+}
+const plus = document.getElementById("plus--id");
+const minus = document.getElementById("minus--id");
+const button = document.getElementById("accordion-button-1");
+const para1 = document.getElementById("ans-1");
+
+button.addEventListener("click", function() {
+  toggleCount();
+  if (count === -1) {
+    plus.classList.add("hide");
+    minus.classList.add("show");
+    para1.classList.add("show");
+  }
+  else {
+    minus.classList.remove("show");
+    plus.classList.remove("hide");
+    para1.classList.remove("hide")
+  }
+})
+
+
+
+
+
+  
