c2f31ce0653e7e42e69b8cd371587659c489100d
Marvellous Bankole <152821703+lightcod3s@users.noreply.github.com>
2024-02-05 04:45:12 +0100

    js added

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// const items = document.querySelectorAll(".accordion button");
-
-// function toggleAccordion() {
-//   const itemToggle = this.getAttribute('aria-expanded');
-
-//   for (i = 0; i < items.length; i++) {
-//     items[i].setAttribute('aria--expanded', 'false');
-//   }
-
-//   if (itemToggle == 'false') {
-//     this.setAttribute('aria-expanded', 'true')
-//   }
-// }
-
-// items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 let count = 1;
 
@@ -39,7 +24,76 @@ button.addEventListener("click", function() {
 })
 
 
+let countB = 1;
+
+function toggleCount() {
+  countB = (countB === 1) ? -1 : 1;
+}
+const plus2 = document.getElementById("plus--id2");
+const minus2 = document.getElementById("minus--id2");
+const button2 = document.getElementById("accordion-button-2");
+const para2 = document.getElementById("ans-2");
+
+button2.addEventListener("click", function() {
+  toggleCount();
+  if (countB === -1) {
+    plus2.classList.add("hide");
+    minus2.classList.add("show");
+    para2.classList.add("show");
+  }
+  else {
+    minus2.classList.remove("show");
+    plus2.classList.remove("hide");
+    para2.classList.remove("show");
+  }
+})
 
 
+let countC = 1;
 
-  
+function toggleCount() {
+  countC = (countC === 1) ? -1 : 1;
+}
+const plus3 = document.getElementById("plus--id3");
+const minus3 = document.getElementById("minus--id3");
+const button3 = document.getElementById("accordion-button-3");
+const para3 = document.getElementById("ans-3");
+
+button3.addEventListener("click", function() {
+  toggleCount();
+  if (countC === -1) {
+    plus3.classList.add("hide");
+    minus3.classList.add("show");
+    para3.classList.add("show");
+  }
+  else {
+    minus3.classList.remove("show");
+    plus3.classList.remove("hide");
+    para3.classList.remove("show");
+  }
+})
+
+
+let countD = 1;
+
+function toggleCount() {
+  countD = (countD === 1) ? -1 : 1;
+}
+const plus4 = document.getElementById("plus--id4");
+const minus4 = document.getElementById("minus--id4");
+const button4 = document.getElementById("accordion-button-4");
+const para4 = document.getElementById("ans-4");
+
+button4.addEventListener("click", function() {
+  toggleCount();
+  if (countD === -1) {
+    plus4.classList.add("hide");
+    minus4.classList.add("show");
+    para4.classList.add("show");
+  }
+  else {
+    minus4.classList.remove("show");
+    plus4.classList.remove("hide");
+    para4.classList.remove("show");
+  }
+})
